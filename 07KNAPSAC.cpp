5b479820a63397e2246dfad3e8dae769fcf2066f
Abhiyb <115642405+Abhiyb@users.noreply.github.com>
2023-08-13 14:11:02 +0530

    Update 07KNAPSAC.cpp

--- 07KNAPSAC.cpp
+++ 07KNAPSAC.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int knapsackDP(vector<int>& weight, vector<int>& value, int capacity) {
+    int n = weight.size();
+    vector<vector<int>> dp(n + 1, vector<int>(capacity + 1, 0));
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= capacity; ++j) {
+            int nottake = dp[i - 1][j];
+            int take = 0;  
+
+            if (weight[i - 1] <= j) {
+                take = dp[i - 1][j - weight[i - 1]] + value[i - 1];
+            }
+
+            dp[i][j] = max(take, nottake);  
+        }
+    }
+
+    return dp[n][capacity];
+}
+
+
+int main() {
+    vector<int> values = {25,20,15,40,50};
+    vector<int> weights = {3,2,1,4,5};
+    int capacity = 6;
+
+    int maxValue = knapsackDP(weights, values, capacity);
+    cout << "Maximum value that can be achieved: " << maxValue << endl;
+
+    return 0;
+}
 

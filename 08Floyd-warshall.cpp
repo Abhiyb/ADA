ff6a7d96cd022baa44a39b013c0a675140e6b949
Abhiyb <115642405+Abhiyb@users.noreply.github.com>
2023-08-24 20:26:58 +0530

    Update 08Floyd-warshall.cpp

--- 08Floyd-warshall.cpp
+++ 08Floyd-warshall.cpp
@@ -6,14 +6,14 @@ void shortest_distance(vector<vector<int>>& matrix) {
     const int INF = 1e9; // Use a very large value for unreachable paths
 
     for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][k] != INF && matrix[k][j] != INF)
-                    matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+     for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][k] != INF && matrix[k][j] != INF)
+             matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
             }
-        }
-    }
-}
+          }
+       }
+   }
 
 int main() {
     int V = 4;
@@ -24,9 +24,7 @@ int main() {
         {5, INF,0, 1},
         {2, INF, INF, 0}
     };
-
     shortest_distance(matrix);
-
     for (auto row : matrix) {
         for (auto cell : row) {
             if (cell == INF)
@@ -36,6 +34,5 @@ int main() {
         }
         cout << endl;
     }
-
     return 0;
 }

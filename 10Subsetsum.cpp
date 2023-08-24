fcfaf06c676dfe775af968e6bc253dc8e2ecbbbc
Abhiyb <115642405+Abhiyb@users.noreply.github.com>
2023-08-24 20:35:03 +0530

    Update 10Subsetsum.cpp

--- 10Subsetsum.cpp
+++ 10Subsetsum.cpp
@@ -8,16 +8,11 @@ void findSubsetsWithSum(vector<int>& nums, int targetSum, int index, int current
         ans.push_back(ds);
         return;
     }
-
     if (index >= nums.size() || currentSum > targetSum) {
         return;
     }
-
-    // Include the current element in the subset
     ds.push_back(nums[index]);
     findSubsetsWithSum(nums, targetSum, index + 1, currentSum + nums[index], ds, ans);
-
-    // Exclude the current element from the subset
     ds.pop_back();
     findSubsetsWithSum(nums, targetSum, index + 1, currentSum, ds, ans);
 }
@@ -27,15 +22,12 @@ int main() {
     int targetSum = 9;
     vector<int> ds;
     vector<vector<int>> ans;
-
     findSubsetsWithSum(nums, targetSum, 0, 0, ds, ans);
-
-    for (const vector<int>& subset : ans) {
+    for (const vector<int>& subset : ans) {  //OR for(auto subset:ans)
         for (int num : subset) {
             cout << num << " ";
         }
         cout << endl;
     }
-
     return 0;
 }

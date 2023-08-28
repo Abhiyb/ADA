ac7a72cbf0711244e2b7e5ae732d5307bd6ceff1
Abhiyb <115642405+Abhiyb@users.noreply.github.com>
2023-08-28 22:58:38 +0530

    Update 05Toposort.cpp

--- 05Toposort.cpp
+++ 05Toposort.cpp
@@ -1,37 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void dfs(vector<bool>& visited, vector<int> adj[], int curr, vector<int>& ans) {
-    visited[curr] = true;
-    for (auto x : adj[curr]) {
-        if (!visited[x]) {
-            dfs(visited, adj, x, ans);
-        }
-    }
-    ans.push_back(curr);
+void dfs(vector<bool>&visited,vector<int>adj[],int curr,stack<int>&st){
+
+visited[curr]=true;
+for(auto x:adj[curr]){
+if(!visited[x]){
+dfs(visited,adj,x,st);
+}
+}
+st.push(curr);
 }
 
-int main() {
-    int n, edges, u, v;
-    cout << "Enter the number of nodes and number of edges: ";
-    cin >> n >> edges;
-
-    vector<bool> visited(n, false);
-    vector<int> adj[n];
-    for (int i = 0; i < edges; i++) {
-        cout << "Enter u and v: ";
-        cin >> u >> v;
-        adj[u].push_back(v);
-    }
-    vector<int> ans;
-    for (int i = 0; i < n; i++) {
-        if (!visited[i]) {
-            dfs(visited, adj, i, ans);
-        }
-    }
-    reverse(ans.begin(), ans.end());
-    for (auto x : ans) {
-        cout << x << " ";
-    }
-    return 0;
+int main(){
+int n,edges,u,v;
+cout<<"Enter the number of nodes and number of edges";
+cin>>n>>edges;
+vector<bool>visited(n,false);
+vector<int>adj[n];
+for(int i=0;i<edges;i++){
+cout<<"Enter u and v";
+cin>>u>>v;
+adj[u].push_back(v);
+}
+stack<int>st;
+dfs(visited,adj,0,st);
+while(!st.empty()){
+cout<<st.top();
+st.pop();
+}
 }
